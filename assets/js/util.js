9a798f4ece61430e4fb48c728873715652ed4b8c
Subash Pathak <subash@subash.me>
2017-09-17 23:51:58 +0545

    Prettify

--- assets/js/util.js
+++ assets/js/util.js
@@ -20,33 +20,34 @@ export function mergeImages(imageUrls, { width, height }) {
   return new Promise((resolve, reject) => {
     const imageSize = { width, height };
     const canvas = createCanvas(imageSize);
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
     const images = imageUrls.map(imageUrl => createImage(imageUrl, imageSize));
     Promise.all(images)
       .then(images => {
-        for(const image of images) {
+        for (const image of images) {
           ctx.drawImage(image, 0, 0, imageSize.width, imageSize.height);
         }
-        resolve(canvas.toDataURL("image/jpeg"));
-      }).catch(reject);
-  })
+        resolve(canvas.toDataURL('image/jpeg'));
+      })
+      .catch(reject);
+  });
 }
 
 export function apiRequest(url, options) {
-  return new Promise((resolve, reject)=> {
+  return new Promise((resolve, reject) => {
     fetch(url, options)
-    .then(response => {
-      if (response.status >= 200 && response.status < 300) {
-        resolve();
-      } else {
-        const error = new Error(response.statusText)
-        error.response = response
-        reject(error);
-      }
-    })
-    .catch(err => {
-      err = err.data ? err : new Error('An error occurred while connecting.');
-      reject(err);
-    });
+      .then(response => {
+        if (response.status >= 200 && response.status < 300) {
+          resolve();
+        } else {
+          const error = new Error(response.statusText);
+          error.response = response;
+          reject(error);
+        }
+      })
+      .catch(err => {
+        err = err.data ? err : new Error('An error occurred while connecting.');
+        reject(err);
+      });
   });
 }
--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 9000;
 //Serve public directory
 app.use(express.static(path.resolve(__dirname, '../', 'public')));
 
-//Proxy profile picture to get around CORS
-app.get('/profile-picture/:userId', (req, res, next)=> {
+//Proxy profile picture to get around same origin policy
+app.get('/profile-picture/:userId', (req, res, next) => {
   const userId = req.params.userId;
   const profilePictureUrl = `http://graph.facebook.com/${userId}/picture?width=800&height=800`;
   const proxy = request.get(profilePictureUrl);
